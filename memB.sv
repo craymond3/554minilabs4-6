50903f8d82406b6802336e5c13e4a9b3b5124a07
Colton Raymond <55898834+craymond3@users.noreply.github.com>
2021-09-27 17:15:33 -0500

    As of end of last Thursday lab

--- memB.sv
+++ memB.sv
@@ -9,4 +9,17 @@ module memB
     output signed [BITS_AB-1:0] Bout [DIM-1:0]
     );
 		
+		logic signed [BITS_AB-1:0] d_in [DIM-1:0];
+		logic signed [BITS_AB-1:0] q_out [DIM-1:0];
+		
+		genvar i;
+		
+		{3{r_VAL_1}});
+		generate
+		for (i = 0; i < DIM; i = i + 1) begin
+			fifo #(.BITS(BITS_AB), .DEPTH(DIM * 2 - 1)) b_fifo(.clk(clk), .rst_n(rst_n), 
+			.en(en), .d({DIM-1-i{1'b0}},Bin[i] >> i * BITS_AB, {i{1'b0}}), .q()); 
+		end
+	 endgenerate
+		
 endmodule
