59e5eb80b4158ae3566ee733dc4dc1f8577e7c2a
cdupont2 <cdupont2@wisc.edu>
2021-09-28 16:27:13 -0500

    added instantiation of other modules

--- tpuv1.sv
+++ tpuv1.sv
@@ -12,7 +12,50 @@ module tpuv1
     output [DATAW-1:0] dataOut,
     input [ADDRW-1:0] addr
    );
-	 
+   
+   
+   
+	
+	memA #(
+		.BITS_AB(BITS_AB),
+		.DIM(DIM)
+	) mem_a(
+		.clk(),
+		.rst_n(),
+		.en(),
+		.WrEn(),
+		.Ain(),
+		.Arow(),
+		.Aout()
+	);
+		
+	memB #(
+		.BITS_AB(BITS_AB),
+		.DIM(DIM)
+	) mem_b(
+		.clk(),
+		.rst_n(),
+		.en(),
+		.bin(),
+		.bout()
+	);
+		
+	systolic_array #(
+		.BITS_AB(BITS_AB),
+		.BITS_C(BITS_C),
+		.DIM(DIM)
+	) sys_array(
+		.clk(),
+		.rst_n(),
+		.WrEn(),
+		.en(),
+		.A(),
+		.B(),
+		.Cin(),
+		.Crow(),
+		.Cout()
+	);
+	
 	 
 	 
 	 
